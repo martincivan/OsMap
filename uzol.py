9e2bbd006f49d27ab7af6721099857d72c1afcb8
Martin Civan <martin.civan5@gmail.com>
2018-06-10 20:31:05 +0200

    vyhladavanie stromom - nehlada :(

---
+++ uzol.py
@@ -0,0 +1,10 @@
+class Uzol:
+
+    def __init__(self, nazov):
+        self.nazov = nazov
+        self.typy = set()
+        self.zaznamy = {}
+
+    def pridajzaznam(self, zaznam):
+        self.typy.add(zaznam.typ)
+        self.zaznamy[zaznam.typ] = zaznam
--- zaznam.py
+++ zaznam.py
@@ -2,20 +2,12 @@
 
 class Zaznam:
 
-    def __init__(self, z, kon):
-        self.typ = z.tag
-        self.atrib = z.attrib
-        self.kont = kon
-
-    @property
-    def nazov(self):
-        return self.atrib['name']
-
-    def pekny_nazov(self):
-        bodka = self.nazov.find('.')
-        if bodka > 0:
-            vysledok = self.nazov[int(0) : int(bodka)]
-            vysledok = vysledok.lower()
-            vysledok = vysledok.replace(self.typ, '')
-            vysledok = vysledok.replace('_', ' ')
-            return vysledok.strip().title()
+    def __init__(self, zaznam):
+        self.typ = zaznam.attrib['type']
+        self.popis = zaznam.attrib['description']
+        self.mbzipu = zaznam.attrib['size']
+        self.mbsuboru = zaznam.attrib['targetsize']
+        self.velkost = zaznam.attrib['contentSize']
+        self.velkostzipu = zaznam.attrib['containerSize']
+        self.cas = zaznam.attrib['timestamp']
+        self.subor = zaznam.attrib['name']
